72d929dfe7cc7a4d6446426710620f3c65ce3a4a
Danpeo <danilvar4@gmail.com>
2023-11-09 18:45:59 +0400

    Add project files.

---
+++ Danilvar.Entity/Entity.cs
@@ -0,0 +1,44 @@
+﻿namespace Danilvar.Entity;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public string Id { get; private init; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid().ToString("N");
+    }
+
+    public static bool operator ==(Entity? right, Entity? left) =>
+        right is not null && left is not null && right.Equals(left);
+
+    public static bool operator !=(Entity? right, Entity? left) =>
+        !(right == left);
+
+    public bool Equals(Entity? other)
+    {
+        if (other is null)
+            return false;
+
+        if (other.GetType() != GetType())
+            return false;
+
+        return other.Id == Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+            return false;
+
+        if (obj.GetType() != GetType())
+            return false;
+
+        if (obj is not Entity entity)
+            return false;
+
+        return entity.Id == Id;
+    }
+
+    public override int GetHashCode() => Id.GetHashCode();
+}
